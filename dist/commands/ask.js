f09a828fe983e9e1f0cb44b9169c85a36e0e4d6f
Hero E <heroemenalom73@gmail.com>
2023-04-30 13:16:23 +0000

    update

--- dist/commands/ask.js
+++ dist/commands/ask.js
@@ -15,24 +15,53 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('ask')
         .setDescription('Ask a question across discord')
-        .addStringOption((option) => option.setName('question').setDescription('A Question').setRequired(true)),
+        .addStringOption((option) => option.setName('question').setDescription('A Question').setRequired(true))
+        .addStringOption((option) => option.setName('subject').setDescription('What is your question generally about?').setRequired(true).addChoices({ name: 'Web Development', value: 'webdev' }, { name: 'Game Development', value: 'gamedev' }, { name: 'Networking', value: 'networking' })),
     execute(interaction) {
-        var _a;
+        var _a, _b, _c;
         return __awaiter(this, void 0, void 0, function* () {
-            const askEmbed = new discord_js_1.EmbedBuilder()
-                .setDescription(`**Question Asked**: ${(_a = interaction.options.get('question')) === null || _a === void 0 ? void 0 : _a.value}`)
+            // params passed by user
+            const question = (_a = interaction.options.get('question')) === null || _a === void 0 ? void 0 : _a.value;
+            const subject = (_b = interaction.options.get('subject')) === null || _b === void 0 ? void 0 : _b.value;
+            const askEmbed = new discord_js_1.EmbedBuilder() // embed confirming user's query
+                .setDescription(`**Question Asked**: ${question}`)
                 .setColor(0xffab2e)
-                .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL() });
-            const sendBtn1 = new discord_js_1.ButtonBuilder()
+                .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL() })
+                .setFooter({ text: subject });
+            const sendBtn1 = new discord_js_1.ButtonBuilder() // send only to server btn
                 .setCustomId('send-only')
                 .setLabel('Send Here')
                 .setStyle(discord_js_1.ButtonStyle.Primary);
-            const sendBtn2 = new discord_js_1.ButtonBuilder()
+            const sendBtn2 = new discord_js_1.ButtonBuilder() // send everywhere btn
                 .setCustomId('send-everywhere')
                 .setLabel('Send Everywhere')
                 .setStyle(discord_js_1.ButtonStyle.Primary);
-            const row = new discord_js_1.ActionRowBuilder().addComponents(sendBtn1, sendBtn2);
-            yield interaction.reply({ embeds: [askEmbed], components: [row], ephemeral: true });
+            const cancelBtn = new discord_js_1.ButtonBuilder() // cancel btn
+                .setCustomId('cancel')
+                .setLabel('Cancel')
+                .setStyle(discord_js_1.ButtonStyle.Secondary);
+            const row = new discord_js_1.ActionRowBuilder().addComponents(sendBtn1, sendBtn2, cancelBtn);
+            const res = yield interaction.reply({ embeds: [askEmbed], components: [row], ephemeral: true });
+            try {
+                const click = yield res.awaitMessageComponent({ filter: u => u.user.id === interaction.user.id });
+                switch (click.customId) {
+                    case 'send-only':
+                        yield click.update({ content: 'only here' });
+                        const channels = yield ((_c = interaction === null || interaction === void 0 ? void 0 : interaction.guild) === null || _c === void 0 ? void 0 : _c.channels.fetch());
+                        const questionCategory = channels.filter((c) => c.name === 'Questions').at(0);
+                        console.log(questionCategory);
+                        break;
+                    case 'send-everywhere':
+                        yield click.update({ content: 'everywhere' });
+                        break;
+                    case 'cancel':
+                        yield click.update({ content: '*Canceled*', components: [] });
+                        break;
+                }
+            }
+            catch (err) {
+                yield interaction.editReply({ content: `error: ${err}` });
+            }
         });
     }
 };
--- src/commands/ask.ts
+++ src/commands/ask.ts
@@ -1,4 +1,4 @@
-import { SlashCommandStringOption, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ButtonComponent } from "discord.js";
+import { SlashCommandStringOption, StringSelectMenuOptionBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Collection, GuildChannel } from "discord.js";
 import { type CommandInteraction } from "discord.js";
 
 const { SlashCommandBuilder } = require('discord.js')
@@ -7,13 +7,20 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('ask')
     .setDescription('Ask a question across discord')
-    .addStringOption((option: SlashCommandStringOption) => option.setName('question').setDescription('A Question').setRequired(true)),
-  async execute(interaction: CommandInteraction){
+    .addStringOption((option: SlashCommandStringOption) => option.setName('question').setDescription('A Question').setRequired(true))
+    .addStringOption((option: SlashCommandStringOption) => option.setName('subject').setDescription('What is your question generally about?').setRequired(true).addChoices({name: 'Web Development', value: 'webdev'},{name: 'Game Development', value: 'gamedev'},{name: 'Networking', value: 'networking'})),
+  async execute(interaction: CommandInteraction) {
+    
+    // params passed by user
+    const question = interaction.options.get('question')?.value;
+    const subject = interaction.options.get('subject')?.value;
+    
 
     const askEmbed = new EmbedBuilder() // embed confirming user's query
-      .setDescription(`**Question Asked**: ${interaction.options.get('question')?.value}`)
+      .setDescription(`**Question Asked**: ${question}`)
       .setColor(0xffab2e)
-      .setAuthor({name: interaction.user.tag, iconURL: interaction.user.avatarURL()!})
+      .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL()! })
+      .setFooter({text: (subject as string)})
 
 
     const sendBtn1 = new ButtonBuilder() // send only to server btn
@@ -24,13 +31,38 @@ module.exports = {
       .setCustomId('send-everywhere')
       .setLabel('Send Everywhere')
       .setStyle(ButtonStyle.Primary);
-    
-    const row = new ActionRowBuilder<ButtonBuilder>().addComponents(sendBtn1,sendBtn2);
-    
+    const cancelBtn = new ButtonBuilder() // cancel btn
+      .setCustomId('cancel')
+      .setLabel('Cancel')
+      .setStyle(ButtonStyle.Secondary);
+
+    const row = new ActionRowBuilder<ButtonBuilder>().addComponents(sendBtn1, sendBtn2,cancelBtn);
+
     const res = await interaction.reply({ embeds: [askEmbed], components: [row], ephemeral: true });
 
-    await res.awaitMessageComponent({ filter: u => u.user.id === interaction.user.id })
-    
-    
+    try {
+      const click = await res.awaitMessageComponent({ filter: u => u.user.id === interaction.user.id })
+
+      switch(click.customId){
+        case 'send-only':
+          await click.update({content: 'only here'})
+          const channels = await interaction?.guild?.channels.fetch()
+          const questionCategory = channels!.filter((c: any) => c.name === 'Questions').at(0) ?? 
+          console.log(questionCategory)
+          
+          break;
+        case 'send-everywhere':
+          await click.update({content: 'everywhere'})
+          break;
+        case 'cancel':
+          await click.update({content: '*Canceled*', components: []})
+          break;
+      }
+      
+    } catch (err) {
+      await interaction.editReply({ content: `error: ${err}` })
+    }
+
+
   }
 }
